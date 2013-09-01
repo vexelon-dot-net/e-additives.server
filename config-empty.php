69fc83129da4dbe73857a8d06860ad3b153f3043
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-09-01 23:16:49 +0200

    Updated config.php defaults and comments.

--- config-empty.php
+++ config-empty.php
@@ -18,17 +18,17 @@ define('DB_SETTINGS', serialize(array(
 ));
 
 // Location settings
+// Note: Add any sub paths required here. Omit slash at the end.
 define('BASE_URL', sprintf("http://%s", $_SERVER["HTTP_HOST"]));
 
 // Runtime settings
 define('MAINTENANCE_MODE', false); // service is offline
-define('DEBUG', true); // spit more log messages (useful for debugging)
-define('SHOW_SQL', true); // spit all sql statements into log
+define('DEBUG', false); // spit more log messages (useful for debugging)
+define('SHOW_SQL', false); // spit all sql statements into log
 
 // Set timezone
 date_default_timezone_set('Europe/Sofia');
 
 // Set time locale
 setlocale(LC_TIME, "en_US");
-
 ?>
