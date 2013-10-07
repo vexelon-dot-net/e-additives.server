7da58f50e7398983579cfcc8d3027b6627fda680
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-10-07 17:25:51 +0200

    Added bg translations of additive categories

--- data/db_app_data.sql
+++ data/db_app_data.sql
@@ -50,29 +50,30 @@ INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, las
 SELECT id FROM AdditiveCategory WHERE category='600' INTO @category_id;
 INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'flavour enhancers', 'Flavor enhancers enhance a food\'s existing flavors. They may be extracted from natural sources (through distillation, solvent extraction, maceration, among other methods) or created artificially.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='700' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'antibiotics', 'Antibiotics are substances or compounds that kills bacteria or inhibit its growth. They belong to the broader group of antimicrobial  compounds, used to treat infections caused by microorganisms, including fungi  and protozoa.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'antibiotics', 'Antibiotics are substances or compounds that kills bacteria or inhibit its growth. They belong to the broader group of antimicrobial  compounds, used to treat infections caused by microorganisms, including fungi and protozoa.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='900' INTO @category_id;
 INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'miscellaneous', 'A variety of other additives, such as sweeteners which are used to replace sugars, may be added to foods.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='1000' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'others', 'No information available.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'other chemicals', 'No information available.', NOW());
 
 # Additive category properties: BG
 SELECT id FROM Locale WHERE code='bg' INTO @locale_id;
 SELECT id FROM AdditiveCategory WHERE category='100' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'оцветители', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'оцветители', 'Група природни или синтетични бои, които се добавят към храната, за да придобие по-добър вид и да изглежда по-апетитно.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='200' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'консерванти', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'консерванти', 'Консерванти са вещества, които служат за предотвратяване и намаляване на разграждането и/или развалянето на продукта в следствие микробно развитие и/или нежелани химични промени.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='300' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'антиоксиданти и киселинни регулатори', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'антиоксиданти и киселинни регулатори', 'Антиоксидантите (са група биохимични вещества, действащи благоприятно и отпускащо върху човешкия организъм. Те са нискомолекулни съединения, попадащи в организма чрез различни хранителни продукти. Основното свойство на тези вещества е унищожаването на свободните радикали в организма, като токсини, тежки метали, токсични съединения, които отделят отровни оксиди и застрашават организма от пагубни болести.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='400' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'сгъстители, стабилизатори и емулгатори', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'сгъстители, стабилизатори и емулгатори', 'Сгъстителите са субстанции, които увеличават вискозитет на дадена смес без да изменят (значително) нейните свойства.
+Емулгаторите са вещества, добавяни към хранителни продукти, с цел получаване на финодисперсионни и устойчиви колоидни системи. Свързват се неограничено във вода, багрят се от водоразтворими багри и са електропроводими.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='500' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'киселинни регулатори и противовтвърдители', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'киселинни регулатори и противовтвърдители', 'Киселинните регулатори се използват, за да се промени или регулира киселинността или алкалността на храните. Противовтвърдителите (антислепващите агенти) предотвратяват слепването на прахове (мляко на прах) от втвърядеване или слепване.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='600' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'ароматизатори', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'ароматизатори', 'Ароматизаторите са вещества, които се използват за предаване на специфичен аромат на храните. Обикновено се извличат от натурални източници чрез дистилация, екстракция чрез разтвори или накисване или биват създавани изкуствено.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='700' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'антибиотици', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'антибиотици', 'Антибиотик е вещество или съединение с подчертан биологичен ефект, изразен в унищожаването или забавянето на растежа на микроорганизми, като бактерии,гъби и протозои.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='900' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'разни', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'разни', 'Различни по вид добавки като подсладители, които са използват като заместители на захарта.', NOW());
 SELECT id FROM AdditiveCategory WHERE category='1000' INTO @category_id;
-INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'други', 'Няма информация.', NOW());
+INSERT INTO AdditiveCategoryProps(category_id, locale_id, name, description, last_update) VALUES(@category_id, @locale_id, 'други химикали', 'Няма информация.', NOW());
